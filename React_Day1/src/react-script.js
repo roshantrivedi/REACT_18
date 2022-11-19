b5032cb43418eebe5a4f7dd8d1e1b964950e26c0
roshantrivedi <42265351+roshantrivedi@users.noreply.github.com>
2022-11-19 19:51:03 +0530

    Hello World
    Starting with "Hello World" in plain HTML, JS & React.

---
+++ React_Day1/src/react-script.js
@@ -0,0 +1,3 @@
+const element2 = React.createElement("h3", {}, "Hello React");
+const root = ReactDOM.createRoot(document.getElementById("react-root"));
+root.render(element2);
---
+++ React_Day1/src/script.js
@@ -0,0 +1,4 @@
+const x = document.getElementById("js-root");
+const element = document.createElement("h3");
+element.innerText = "Hello World [using JS]";
+x.appendChild(element);
